11d7dcc656c6764f711647f85bfe62be6bff14ea
millsoftspb <millsoftspb@gmail.com>
2019-08-17 12:46:20 +0300

    add temp instruction video

--- app/src/main/java/com/wincomp/rj45colors/Instruction_Activity.java
+++ app/src/main/java/com/wincomp/rj45colors/Instruction_Activity.java
@@ -1,7 +1,10 @@
 package com.wincomp.rj45colors;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Instruction_Activity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Instruction_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction_);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String videopath = "android.resource://" + getPackageName() + "/" + R.raw.instruction1;
+        Uri uri = Uri.parse(videopath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
     }
 }
